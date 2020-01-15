a8324660a2c808f0e7be018f510f028e891d91a9
Diogo Silva <imdiogosilva@gmail.com>
2020-01-15 20:55:00 +0000

    Cathcing Uncaught Exceptions

--- final-project/server.js
+++ final-project/server.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
+
+process.on('uncaughtException', err => {
+  console.log('Unhandled Exception! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('Unhandled Rejection! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
