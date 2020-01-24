f59bd3a05096dad7b1a6aafef768f9fe7b3fc383
Diogo Silva <diogosilva@Diogos-MacBook-Pro.local>
2020-01-24 21:29:32 +0000

    Signing in Users

--- final-project/controllers/authController.js
+++ final-project/controllers/authController.js
@@ -1,10 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name: name,
+    email: email,
+    password: password,
+    passwordConfirm: passwordConfirm
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
   res.status(201).json({
     status: 'sucess',
+    token,
     data: {
       user: newUser
     }
