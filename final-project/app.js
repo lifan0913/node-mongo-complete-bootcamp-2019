ffcebdab23761bbac5e24757765a6187f27c0bbd
Diogo Silva <imdiogosilva@gmail.com>
2020-02-10 22:01:26 +0000

    Setting Security HTTP Headers

--- final-project/app.js
+++ final-project/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,19 +11,26 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP to the API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again later!'
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Our own Middlewares
