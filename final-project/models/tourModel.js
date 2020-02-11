c1ee86c8d9d01365b849cd606c35abaaa5241c95
Diogo Silva <imdiogosilva@gmail.com>
2020-02-11 17:26:36 +0000

    Modelling Tour Guides: Child Referencing

--- final-project/models/tourModel.js
+++ final-project/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,7 +102,12 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -120,11 +125,12 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
+// Modelling Tour Guides: Embedding
+/* tourSchema.pre('save', async function(next) {
   const guidesPromises = this.guides.map(async id => await User.findById(id));
   this.guides = await Promise.all(guidesPromises);
   next();
-});
+}); */
 
 // QUERY MIDDLEWARE: Runs before .find()
 //tourSchema.pre('find', function(next) {
