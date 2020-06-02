e144858bd426b143890ab98f6054cc9a78d26e83
Diogo Silva <imdiogosilva@gmail.com>
2020-06-02 17:59:53 +0100

    Building Tour Overview

--- final-project/controllers/viewsController.js
+++ final-project/controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1 get all tour data from collections
+  const tours = await Tour.find();
+  // 2 Build template
+  // 3 Render Template using tour data
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
