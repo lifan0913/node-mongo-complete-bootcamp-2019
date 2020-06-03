3850b7dca7fce8ce9bc319ad72b630ea523078fa
Diogo Silva <imdiogosilva@gmail.com>
2020-06-03 20:17:57 +0100

    Building Tour Page

--- final-project/controllers/viewsController.js
+++ final-project/controllers/viewsController.js
@@ -2,18 +2,22 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
-  // 1 get all tour data from collections
   const tours = await Tour.find();
-  // 2 Build template
-  // 3 Render Template using tour data
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  console.log(tour);
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour
+  });
+});
